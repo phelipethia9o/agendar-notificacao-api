eb7db5c881a5a7f9b411abb35153c9319588d0ee
phelipethia9o <thiagowiski.psn@gmail.com>
2025-02-12 16:14:28 -0300

    commit inicial

---
+++ agendamento-de-notificacao/src/test/java/com/estudos/agendamento_de_notificacao/AgendamentoDeNotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.agendamento_de_notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoDeNotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
