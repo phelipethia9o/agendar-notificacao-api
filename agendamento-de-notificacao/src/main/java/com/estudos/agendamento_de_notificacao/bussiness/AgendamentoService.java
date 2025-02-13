adc1a60877e76739685889621492686b17164061
phelipethia9o <thiagowiski.psn@gmail.com>
2025-02-12 21:05:22 -0300

    adcionado o metodo de agendamento de notificacao

---
+++ agendamento-de-notificacao/src/main/java/com/estudos/agendamento_de_notificacao/bussiness/AgendamentoService.java
@@ -0,0 +1,24 @@
+package com.estudos.agendamento_de_notificacao.bussiness;
+
+
+import com.estudos.agendamento_de_notificacao.bussiness.mapper.IAgendamentoMapper;
+import com.estudos.agendamento_de_notificacao.controller.dto.in.AgendamentoRecord;
+import com.estudos.agendamento_de_notificacao.controller.dto.out.AgendamentoRecordOut;
+
+import com.estudos.agendamento_de_notificacao.infrastructure.repositories.AgendamentoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AgendamentoService {
+
+    private final AgendamentoRepository repository;
+    private final IAgendamentoMapper agendamentoMapper;
+
+    public AgendamentoRecordOut gravarAgendamento(AgendamentoRecord agendamento){
+        return agendamentoMapper.paraOut(
+                repository.save(
+                        agendamentoMapper.paraEntity(agendamento)));
+    }
+}
---
+++ agendamento-de-notificacao/src/main/java/com/estudos/agendamento_de_notificacao/infrastructure/enums/StatusNotificacaoEnum.java
@@ -0,0 +1,8 @@
+package com.estudos.agendamento_de_notificacao.infrastructure.enums;
+
+public enum StatusNotificacaoEnum {
+
+    AGENDADO,
+    ENVIADO,
+    CANCELADO
+}
